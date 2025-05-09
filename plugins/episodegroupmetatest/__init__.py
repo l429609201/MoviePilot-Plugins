691ff3e331c3264c5fd22e3e661641bf1d055753
MY RAILGUN <87714508+l429609201@users.noreply.github.com>
2025-05-10 07:23:15 +0800

    Update __init__.py

--- plugins/episodegroupmetatest/__init__.py
+++ plugins/episodegroupmetatest/__init__.py
@@ -50,7 +50,7 @@ class EpisodeGroupMetaTest(_PluginBase):
     # 插件作者
     plugin_author = "AAA"
     # 作者主页
-    author_url = "https://github.com/cikezhu"
+    author_url = "https://github.com/l429609201"
     # 插件配置项ID前缀
     plugin_config_prefix = "EpisodeGroupMetaTest_"
     # 加载顺序
