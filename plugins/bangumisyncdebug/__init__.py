f66a729d7f8014d851607558c92cf86deefd7ae2
MY RAILGUN <87714508+l429609201@users.noreply.github.com>
2025-05-14 16:12:54 +0800

    Update __init__.py

--- plugins/bangumisyncdebug/__init__.py
+++ plugins/bangumisyncdebug/__init__.py
@@ -95,12 +95,9 @@ class BangumiSyncDebug(_PluginBase):
                     
                 #logger.debug(f"收到webhook事件: {event.event_data}")
                 item_data=webhook_data.get("Item") 
-                logger.debug(f"转换前的season_id: {item_data.get("ParentIndexNumber")}")
-                logger.debug(f"转换前的episode_id: {item_data.get("IndexNumber")}")
                 # 季 集
                 #season_id, episode_id = map(int, [item_data.get("ParentIndexNumber"), item_data.get("IndexNumber")])
                 tmdb_id = item_data.get("ProviderIds", {}).get("Tmdb")
-                tmdb_id = int(tmdb_id) if tmdb_id is not None else None
                 season_id = int(item_data.get("ParentIndexNumber"))
                 episode_id = int(item_data.get("IndexNumber"))
                 logger.debug(f"转换后的season_id: {tmdb_id}")
